ad1ac03329bebe9433753a9630463aaf2fc3e3df
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-09-07 10:35:29 +0200

    Introduced the power package

---
+++ power/README.md
@@ -0,0 +1,4 @@
+# Power
+
+The package provides algorithms for simulating the power dissipation of
+concurrent applications running on multiprocessor platforms.
---
+++ power/main.go
@@ -0,0 +1,3 @@
+// Package power provides algorithms for simulating the power dissipation of
+// concurrent applications running on multiprocessor platforms.
+package power
