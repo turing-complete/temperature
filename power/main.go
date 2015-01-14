3bf6a09f0b25ff05b4623f0b69ac5000acc8bed0
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-01-14 15:27:57 +0100

    Renamed Distributer into Power

--- power/main.go
+++ power/main.go
@@ -7,8 +7,8 @@ import (
 	"github.com/ready-steady/persim/time"
 )
 
-// Distributer represents a power simulator configured for a particular system.
-type Distributer struct {
+// Power represents a power simulator configured for a particular system.
+type Power struct {
 	platform    *system.Platform
 	application *system.Application
 	Δt          float64
@@ -16,8 +16,8 @@ type Distributer struct {
 
 // New returns a power distributor for the given platform, application, and
 // sampling period.
-func New(platform *system.Platform, application *system.Application, Δt float64) *Distributer {
-	return &Distributer{
+func New(platform *system.Platform, application *system.Application, Δt float64) *Power {
+	return &Power{
 		platform:    platform,
 		application: application,
 		Δt:          Δt,
@@ -28,9 +28,9 @@ func New(platform *system.Platform, application *system.Application, Δt float64
 // cc-by-sc matrix P where cc is the number of cores and sc is the maximal
 // number of steps (samples) that the matrix can accommodate. P is assumed to be
 // zeroed.
-func (self *Distributer) Compute(schedule *time.Schedule, P []float64, sc uint32) {
-	cores, tasks := self.platform.Cores, self.application.Tasks
-	Δt := self.Δt
+func (p *Power) Compute(schedule *time.Schedule, P []float64, sc uint32) {
+	cores, tasks := p.platform.Cores, p.application.Tasks
+	Δt := p.Δt
 
 	cc := uint32(len(cores))
 	tc := uint16(len(tasks))
@@ -38,15 +38,12 @@ func (self *Distributer) Compute(schedule *time.Schedule, P []float64, sc uint32
 		sc = count
 	}
 
-	var j, s, f uint32
-	var p float64
-
 	for i := uint16(0); i < tc; i++ {
-		j = uint32(schedule.Mapping[i])
-		p = cores[j].Power[tasks[i].Type]
+		j := uint32(schedule.Mapping[i])
+		p := cores[j].Power[tasks[i].Type]
 
-		s = uint32(schedule.Start[i] / Δt)
-		f = uint32(schedule.Finish[i] / Δt)
+		s := uint32(schedule.Start[i] / Δt)
+		f := uint32(schedule.Finish[i] / Δt)
 		if f > sc {
 			f = sc
 		}
