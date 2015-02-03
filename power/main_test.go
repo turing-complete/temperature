238f44511f04233e89d930b6917f423892f65074
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-02-03 13:18:57 +0100

    Added a benchmark for power

--- power/main_test.go
+++ power/main_test.go
@@ -15,11 +15,9 @@ const (
 
 func TestCompute(t *testing.T) {
 	platform, application, _ := system.Load(findFixture("002_040.tgff"))
-
 	profile := system.NewProfile(platform, application)
 	list := time.NewList(platform, application)
 	schedule := list.Compute(profile.Mobility)
-
 	power, _ := New(platform, application, 1e-3)
 
 	P := make([]float64, 2*440)
@@ -29,6 +27,27 @@ func TestCompute(t *testing.T) {
 	P = make([]float64, 2*42)
 	power.Compute(schedule, P, 42)
 	assert.Equal(P, fixturePData[:2*42], t)
+}
+
+func BenchmarkCompute(b *testing.B) {
+	const (
+		Δt = 1e-5
+	)
+
+	platform, application, _ := system.Load(findFixture("002_040.tgff"))
+	profile := system.NewProfile(platform, application)
+	list := time.NewList(platform, application)
+	schedule := list.Compute(profile.Mobility)
+	power, _ := New(platform, application, Δt)
+
+	sc := uint32(schedule.Span / Δt)
+	P := make([]float64, 2*sc)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		power.Compute(schedule, P, sc)
+	}
 }
 
 func findFixture(name string) string {
