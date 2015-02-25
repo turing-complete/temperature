c1dd1afaf682b840cc0d0dd45b0fdd958e4c0a4b
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-02-25 11:23:39 +0100

    Cosmetic adjustments in analytic.Compute

--- temperature/analytic/transient.go
+++ temperature/analytic/transient.go
@@ -9,24 +9,21 @@ import (
 // profile will contain; if the power profile contains more samples than needed,
 // it will be truncated accordingly.
 func (t *Temperature) Compute(P []float64, sc uint) []float64 {
-	cc := t.Cores
-	nc := t.Nodes
+	cc, nc := t.Cores, t.Nodes
 
 	Q := make([]float64, cc*sc)
 	S := make([]float64, nc*sc)
 
 	matrix.Multiply(t.system.F, P, S, nc, cc, sc)
 
-	var i, j, k uint
-
-	for i, j, k = 1, 0, nc; i < sc; i++ {
+	for i, j, k := uint(1), uint(0), nc; i < sc; i++ {
 		matrix.MultiplyAdd(t.system.E, S[j:k], S[k:k+nc], S[k:k+nc], nc, nc, 1)
 		j += nc
 		k += nc
 	}
 
-	for i = 0; i < cc; i++ {
-		for j = 0; j < sc; j++ {
+	for i := uint(0); i < cc; i++ {
+		for j := uint(0); j < sc; j++ {
 			Q[cc*j+i] = t.system.D[i]*S[nc*j+i] + t.system.Qamb
 		}
 	}
