788dd2ce5759f071404900cf324249b4775ddb2d
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-02-25 13:15:55 +0100

    Added benchmarks for 002 and 032 in numeric

--- temperature/numeric/transient_test.go
+++ temperature/numeric/transient_test.go
@@ -3,25 +3,54 @@ package numeric
 import (
 	"testing"
 
+	"github.com/ready-steady/probability"
+	"github.com/ready-steady/probability/uniform"
 	"github.com/ready-steady/support/assert"
 )
 
 func TestCompute(t *testing.T) {
 	temperature := load("002")
+	cc, sc, Δt := uint(2), uint(440), 1e-3
 
-	cc := uint(2)
-	sc := uint(len(fixtureP)) / cc
-	Δt := temperature.system.Δt
-
-	power := func(time float64, power []float64) {
-		k := uint(time / Δt)
-		for i := uint(0); i < cc; i++ {
-			power[i] = fixtureP[k*cc+i]
-		}
-	}
-
+	power := smooth(fixtureP, cc, sc, Δt)
 	Q, err := temperature.Compute(power, sc)
 
 	assert.Success(err, t)
 	assert.EqualWithin(Q, fixtureQ, 2e-10, t)
+}
+
+func BenchmarkCompute002(b *testing.B) {
+	temperature := load("002")
+	cc, sc, Δt := uint(2), uint(440), 1e-3
+
+	power := smooth(fixtureP, cc, sc, Δt)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		temperature.Compute(power, sc)
+	}
+}
+
+func BenchmarkCompute032(b *testing.B) {
+	temperature := load("032")
+	cc, sc, Δt := uint(32), uint(1000), 1e-3
+
+	P := probability.Sample(uniform.New(0, 20), cc*sc)
+	power := smooth(P, cc, sc, Δt)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		temperature.Compute(power, sc)
+	}
+}
+
+func smooth(P []float64, cc, sc uint, Δt float64) func(float64, []float64) {
+	return func(time float64, power []float64) {
+		k := uint(time / Δt)
+		for i := uint(0); i < cc; i++ {
+			power[i] = P[k*cc+i]
+		}
+	}
 }
