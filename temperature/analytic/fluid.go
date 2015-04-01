46c2b0f9bc7d5423a3abbd4510ea653b2ac196bc
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-04-01 22:07:47 +0200

    temperature/analytic: implement Fluid

---
+++ temperature/analytic/fluid.go
@@ -0,0 +1,113 @@
+package analytic
+
+import (
+	"math"
+
+	"github.com/ready-steady/hotspot"
+	"github.com/ready-steady/linear/matrix"
+)
+
+// Fluid represents an integrator of a thermal system with a fluid time step.
+type Fluid struct {
+	nc uint
+	nn uint
+
+	// D = C**(-½)
+	D []float64
+
+	// A = U * diag(Λ) * U**T
+	U []float64
+	Λ []float64
+
+	qamb float64
+}
+
+// NewFluid returns a new integrator with a fluid time step.
+func NewFluid(config *Config) (*Fluid, error) {
+	model := hotspot.New((*hotspot.Config)(&config.Config))
+	nc, nn := model.Cores, model.Nodes
+
+	// Reusing model.G to store A and model.C to store D.
+	A := model.G
+	D := model.C
+	for i := uint(0); i < nn; i++ {
+		D[i] = math.Sqrt(1 / model.C[i])
+	}
+	for i := uint(0); i < nn; i++ {
+		for j := uint(0); j < nn; j++ {
+			A[j*nn+i] = -D[i] * D[j] * A[j*nn+i]
+		}
+	}
+
+	// Reusing A (which is model.G) to store U.
+	U := A
+	Λ := make([]float64, nn)
+	if err := matrix.SymmetricEigen(A, U, Λ, nn); err != nil {
+		return nil, err
+	}
+
+	temperature := &Fluid{
+		nc: nc,
+		nn: nn,
+
+		D: D,
+
+		Λ: Λ,
+		U: U,
+
+		qamb: config.Ambience,
+	}
+
+	return temperature, nil
+}
+
+// Compute calculates the temperature profile corresponding to a power profile.
+// The power profile is specified by a matrix capturing the power dissipation at
+// a number time intervals.
+func (self *Fluid) Compute(P []float64, time []float64) []float64 {
+	nc, nn, ns := self.nc, self.nn, uint(len(time))
+
+	D, U, Λ, qamb := self.D, self.U, self.Λ, self.qamb
+
+	diag := make([]float64, nn)
+	temp := make([]float64, nn*nn)
+
+	E := make([]float64, nn*nn)
+	F := make([]float64, nn*nc)
+
+	S1 := make([]float64, nn)
+	S2 := make([]float64, nn)
+
+	Q := make([]float64, nc*ns)
+
+	for i := uint(0); i < ns; i++ {
+		Δt := time[i]
+
+		for j := uint(0); j < nn; j++ {
+			diag[j] = math.Exp(Δt * Λ[j])
+			for k := uint(0); k < nn; k++ {
+				temp[k*nn+j] = diag[j] * U[j*nn+k]
+			}
+		}
+		matrix.Multiply(U, temp, E, nn, nn, nn)
+
+		for j := uint(0); j < nn; j++ {
+			diag[j] = (diag[j] - 1) / Λ[j]
+			for k := uint(0); k < nc; k++ {
+				temp[k*nn+j] = diag[j] * U[j*nn+k] * D[k]
+			}
+		}
+		matrix.Multiply(U, temp, F, nn, nn, nc)
+
+		matrix.Multiply(F, P[i*nc:(i+1)*nc], S1, nn, nc, 1)
+		matrix.MultiplyAdd(E, S2, S1, S1, nn, nn, 1)
+
+		for j := uint(0); j < nc; j++ {
+			Q[i*nc+j] = D[j]*S1[j] + qamb
+		}
+
+		S1, S2 = S2, S1
+	}
+
+	return Q
+}
---
+++ temperature/analytic/fluid_test.go
@@ -0,0 +1,104 @@
+package analytic
+
+import (
+	"fmt"
+	"math"
+	"testing"
+
+	"github.com/ready-steady/assert"
+	"github.com/ready-steady/fixture"
+	"github.com/ready-steady/probability"
+	"github.com/ready-steady/probability/uniform"
+)
+
+func TestFluidNew(t *testing.T) {
+	const (
+		nc = 2
+	)
+
+	temperature, _ := loadFluid(nc)
+
+	assert.Equal(temperature.nc, uint(nc), t)
+	assert.Equal(temperature.nn, uint(4*nc+12), t)
+
+	assert.EqualWithin(temperature.D, fixtureD, 1e-14, t)
+
+	assert.EqualWithin(abs(temperature.U), abs(fixtureU), 1e-9, t)
+	assert.EqualWithin(temperature.Λ, fixtureΛ, 1e-9, t)
+}
+
+func TestFluidCompute(t *testing.T) {
+	const (
+		nc = 2
+	)
+
+	temperature, config := loadFluid(nc)
+	ns := uint(len(fixtureP) / nc)
+
+	time := make([]float64, ns)
+	for i := range time {
+		time[i] = config.TimeStep
+	}
+
+	Q := temperature.Compute(fixtureP, time)
+
+	assert.EqualWithin(Q, fixtureQ, 1e-12, t)
+}
+
+func BenchmarkFluidCompute002(b *testing.B) {
+	const (
+		nc = 2
+	)
+
+	temperature, config := loadFluid(nc)
+	ns := uint(len(fixtureP) / nc)
+
+	time := make([]float64, ns)
+	for i := range time {
+		time[i] = config.TimeStep
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		temperature.Compute(fixtureP, time)
+	}
+}
+
+func BenchmarkFluidCompute032(b *testing.B) {
+	const (
+		nc = 32
+		ns = 1000
+	)
+
+	temperature, config := loadFluid(nc)
+	P := probability.Sample(uniform.New(0, 1), nc*ns)
+
+	time := make([]float64, ns)
+	for i := range time {
+		time[i] = config.TimeStep
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		temperature.Compute(P, time)
+	}
+}
+
+func abs(A []float64) []float64 {
+	B := make([]float64, len(A))
+
+	for i := range B {
+		B[i] = math.Abs(A[i])
+	}
+
+	return B
+}
+
+func loadFluid(nc uint) (*Fluid, *Config) {
+	config := &Config{}
+	fixture.Load(findFixture(fmt.Sprintf("%03d.json", nc)), config)
+	temperature, _ := NewFluid(config)
+	return temperature, config
+}
