068423c36a34e0b9fbf04cd0d0cc22daac181f87
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-04-02 21:21:19 +0200

    power: implement Partition

--- power/main.go
+++ power/main.go
@@ -5,6 +5,7 @@ package power
 import (
 	"github.com/ready-steady/simulation/system"
 	"github.com/ready-steady/simulation/time"
+	"github.com/ready-steady/sort"
 )
 
 // Power represents a power simulator configured for a particular system.
@@ -21,9 +22,44 @@ func New(platform *system.Platform, application *system.Application) *Power {
 	}
 }
 
-// Sample computes a power profile of a schedule with a sampling interval Δt.
-// The ns parameter controls the number of samples that the output matrix will
-// contain; short schedules are extended while long ones are truncated.
+// Partition computes a power profile with a variable time step dictated by the
+// time moments of power switches (the start and finish times of the tasks) and
+// a number of additional time moments gathered in points.
+func (p *Power) Partition(schedule *time.Schedule, points []float64,
+	ε float64) ([]float64, []float64, []uint) {
+
+	cores, tasks := p.platform.Cores, p.application.Tasks
+	nc, nt, np := uint(len(cores)), uint(len(tasks)), uint(len(points))
+
+	time := make([]float64, 2*nt+np)
+	copy(time[:nt], schedule.Start)
+	copy(time[nt:], schedule.Finish)
+	copy(time[2*nt:], points)
+
+	ΔT, index := traverse(time, ε)
+	sindex, findex, pindex := index[:nt], index[nt:2*nt], index[2*nt:]
+
+	ns := uint(len(ΔT))
+
+	P := make([]float64, nc*ns)
+
+	for i := uint(0); i < nt; i++ {
+		j := schedule.Mapping[i]
+		p := cores[j].Power[tasks[i].Type]
+
+		s, f := sindex[i], findex[i]
+
+		for ; s < f; s++ {
+			P[s*nc+j] = p
+		}
+	}
+
+	return P, ΔT, pindex
+}
+
+// Sample computes a power profile with respect to a sampling interval Δt. The
+// required number of samples is specified by ns; short schedules are extended
+// while long ones are truncated.
 func (p *Power) Sample(schedule *time.Schedule, Δt float64, ns uint) []float64 {
 	cores, tasks := p.platform.Cores, p.application.Tasks
 	nc, nt := uint(len(cores)), uint(len(tasks))
@@ -52,9 +88,8 @@ func (p *Power) Sample(schedule *time.Schedule, Δt float64, ns uint) []float64 
 	return P
 }
 
-// Progress takes a schedule and returns a function func(time float64, power
-// []float64) that computes the power dissipation at an arbitrary time moment
-// according to the schedule.
+// Progress returns a function func(time float64, power []float64) that computes
+// the power dissipation at an arbitrary time moment according to a schedule.
 func (p *Power) Progress(schedule *time.Schedule) func(float64, []float64) {
 	cores, tasks := p.platform.Cores, p.application.Tasks
 	nc, nt := uint(len(cores)), uint(len(tasks))
@@ -82,4 +117,27 @@ func (p *Power) Progress(schedule *time.Schedule) func(float64, []float64) {
 			}
 		}
 	}
+}
+
+func traverse(points []float64, ε float64) ([]float64, []uint) {
+	np := uint(len(points))
+	order, _ := sort.Quick(points)
+
+	Δ := make([]float64, np-1)
+	index := make([]uint, np)
+
+	j := uint(0)
+
+	for i, x := uint(1), points[0]; i < np; i++ {
+		if δ := points[i] - x; δ < ε {
+			index[order[i]] = index[order[i-1]]
+		} else {
+			Δ[j] = δ
+			j++
+			x = points[i]
+			index[order[i]] = j
+		}
+	}
+
+	return Δ[:j], index
 }
--- power/main_test.go
+++ power/main_test.go
@@ -10,15 +10,31 @@ import (
 	"github.com/ready-steady/simulation/time"
 )
 
-func TestSample(t *testing.T) {
+func TestPartition(t *testing.T) {
 	const (
-		Δt = 1e-3
+		n = 10
+		ε = 1e-14
 	)
 
 	power, schedule := prepare("002_040")
 
-	assert.Equal(power.Sample(schedule, Δt, 440), fixtureP, t)
-	assert.Equal(power.Sample(schedule, Δt, 42), fixtureP[:2*42], t)
+	points := make([]float64, n)
+	for i := range points {
+		points[i] = float64(i+1) * schedule.Span / n
+	}
+	P, ΔT, index := power.Partition(schedule, points, ε)
+
+	assert.Equal(P, fixturePartition.P, t)
+	assert.EqualWithin(ΔT, fixturePartition.ΔT, 1e-15, t)
+	assert.Equal(index, fixturePartition.index, t)
+
+	Σ := 0.0
+	for i, j := uint(0), uint(0); i < n; i++ {
+		for ; j < index[i]; j++ {
+			Σ += ΔT[j]
+		}
+		assert.EqualWithin(Σ, points[i], 1e-15, t)
+	}
 }
 
 func TestProgress(t *testing.T) {
@@ -38,11 +54,52 @@ func TestProgress(t *testing.T) {
 
 	mismatches := 0
 	for i := range P {
-		if P[i] != fixtureP[i] {
+		if P[i] != fixtureSample.P[i] {
 			mismatches++
 		}
 	}
 	assert.Equal(mismatches, 17, t)
+}
+
+func TestSample(t *testing.T) {
+	const (
+		Δt = 1e-3
+	)
+
+	power, schedule := prepare("002_040")
+
+	assert.Equal(power.Sample(schedule, Δt, 440), fixtureSample.P, t)
+	assert.Equal(power.Sample(schedule, Δt, 42), fixtureSample.P[:2*42], t)
+}
+
+func TestTraverse(t *testing.T) {
+	const (
+		ε = 1e-14
+	)
+
+	test := func(points, Δ []float64, index []uint) {
+		a, b := traverse(points, ε)
+		assert.Equal(a, Δ, t)
+		assert.Equal(b, index, t)
+	}
+
+	test(
+		[]float64{0, 1, 2, 3, 4, 1, 2, 3, 4, 5},
+		[]float64{1, 1, 1, 1, 1},
+		[]uint{0, 1, 2, 3, 4, 1, 2, 3, 4, 5},
+	)
+
+	test(
+		[]float64{0, 0, 0, 2, 4, 0, 6, 8},
+		[]float64{2, 2, 2, 2},
+		[]uint{0, 0, 0, 1, 2, 0, 3, 4},
+	)
+
+	test(
+		[]float64{15, 10, 6, 3, 3, 1},
+		[]float64{2, 3, 4, 5},
+		[]uint{4, 3, 2, 1, 1, 0},
+	)
 }
 
 func BenchmarkSample(b *testing.B) {
