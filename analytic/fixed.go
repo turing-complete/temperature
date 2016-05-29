f538ccbf7f40f6ce4cfce54cec0cf7fe814875e9
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-05-29 09:04:27 +0200

    analytic: adjust a comment

--- analytic/fixed.go
+++ analytic/fixed.go
@@ -123,7 +123,7 @@ func (self *Fixed) Compute(P []float64) []float64 {
 
 // ComputeWithStatic calculates the temperature profile and the total power
 // profile corresponding to a dynamic power profile taking into account the
-// leakage power.
+// static power.
 //
 // The dynamic power profile is specified by a matrix P containing power samples
 // at a number of equidistant time moments (see TimeStep in Config). The dynamic
